09e1bf76cef778053d3cf0bbd067db34973688e9
Svitlana Ambros <Svitlana_Ambros@epam.com>
2019-10-24 16:50:07 +0300

    create item list layout

--- src/app/courses/components/courses-item/courses-item.component.ts
+++ src/app/courses/components/courses-item/courses-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Course} from "@courses/models/course.model";
 
 @Component({
   selector: 'app-courses-item',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-item.component.scss']
 })
 export class CoursesItemComponent implements OnInit {
+  @Input() item: Course =
+    {
+      id: '1',
+      title: 'Video Course 1. Name tag',
+      creationDate: new Date(),
+      duration: '1hr',
+      description: 'Learn about where you can find course descriptions,' +
+        ' what information they include, how they work, and details ' +
+        'about various components of a course description. Course descriptions ' +
+        'report information about a university or college\'s classes. They\'re ' +
+        'published both in course catalogs that outline degree requirements and ' +
+        'in course schedules that contain descriptions for all courses offered ' +
+        'during a particular semester.'
+    };
 
   constructor() { }
 
