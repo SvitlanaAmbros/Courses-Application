aeae81af81ad2d2b65d49c6159b030690a40f888
Svitlana_Ambros <svitlana.ambros@refinitiv.com>
2019-11-29 17:45:06 +0200

    minor fix

--- src/app/core/breadcrumbs/breadcrumbs.component.ts
+++ src/app/core/breadcrumbs/breadcrumbs.component.ts
@@ -1,17 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss']
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnChanges {
   @Input() fullPath: string[] = [];
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit() {
     console.log(this.fullPath);
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.router);
   }
 
 }
